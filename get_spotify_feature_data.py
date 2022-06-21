d00e79a384ea94283bb99f52a581e1ef11d6f4cc
Misato Watanabe <watamisa080513@gmail.com>
2022-06-22 00:48:44 +0900

    add dict data

--- get_spotify_feature_data.py
+++ get_spotify_feature_data.py
@@ -9,8 +9,8 @@ import glob
 artist_json_open = open('./data/artist.json', 'r')
 artist_name_id_list = json.load(artist_json_open)
 
-client_id = ''
-client_secret = ''
+client_id = '90e8ab1241af4266a8e5f8306e6e0ed8'
+client_secret = '1431d669ab724aad8c91a737f2a79f60'
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
     client_id=client_id, client_secret=client_secret), requests_timeout=5)
@@ -25,20 +25,49 @@ for filename in sorted(glob.glob("../spotify_chart_data/regional-jp-weekly-*.csv
             id = row["uri"][14:]
             rank = row['\ufeffrank']
             date = filename[-14:-4]
-            if int(rank) < 100:
-                continue
-            song_features = sp.audio_features(id)
             analysis_song_info = dict()
-            analysis_song_info['id'] = id
-            analysis_song_info['title'] = row['track_name']
+
+            if analysis_song_data.get(id):
+                # print(analysis_song_data[id].get("stream"))
+                if analysis_song_data[id].get("stream"):
+                    # print(analysis_song_data[id]["stream"], row["streams"])
+                    if analysis_song_data[id]['stream'] < row["streams"]:
+                        # print(
+                        #     "update", analysis_song_data[id]["stream"], row["streams"])
+                        analysis_song_data[id]['stream'] = row["streams"]
+                else:
+                    analysis_song_data[id]['stream'] = row["streams"]
+
+                if analysis_song_data[id].get("ranking"):
+                    # print(analysis_song_data[id]["stream"], row["streams"])
+                    if analysis_song_data[id]['ranking'] > rank:
+                        # print(
+                        #     "update", analysis_song_data[id]["stream"], row["streams"])
+                        analysis_song_data[id]['ranking'] = rank
+                else:
+                    analysis_song_data[id]['ranking'] = rank
+            else:
+                analysis_song_info['id'] = id
+                analysis_song_info['title'] = row['track_name']
+                analysis_song_info['ranking'] = rank
+                analysis_song_info['date'] = date
+                analysis_song_data[id] = analysis_song_info
+                analysis_song_data[id]['stream'] = row["streams"]
+            # print(
+            #     "make", row['track_name'], analysis_song_data[id]["stream"], row["streams"])
+
+            if int(rank) < 125:
+                continue
+
+            song_features = sp.audio_features(id)
+
             for key in json_key_name:
                 analysis_song_info[key] = song_features[0][key]
-            analysis_song_info['ranking'] = rank
-            analysis_song_info['date'] = date
 
             # add
             artist_name = row["artist_names"]
-            artist_result = sp.search(q='artist:' + artist_name, type='artist')
+            artist_result = sp.search(
+                q='artist:' + artist_name, type='artist')
             artist_info_list = artist_result["artists"]["items"]
             analysis_song_info["artist"] = artist_name
 
@@ -56,12 +85,12 @@ for filename in sorted(glob.glob("../spotify_chart_data/regional-jp-weekly-*.csv
             # for artist_info in artist_info_list:
             #     analysis_song_data["genres"].append(artist_info["genres"])
 
-            analysis_song_data[id] = analysis_song_info
-        print(date, " fin")
+            #analysis_song_data[id] = analysis_song_info
+        print("########", date, " fin")
 
 print(len(analysis_song_data))
 
-with open('./test/analysis-song-data-100-200.json', 'w', encoding='utf-8') as f:
+with open('./test/sample_stream.json', 'w', encoding='utf-8') as f:
     json.dump(analysis_song_data, f, indent=2, ensure_ascii=False)
 
 print("all ok")
